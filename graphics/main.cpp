d2e622d16130b74c6204f1338ae63ef0df9124dc
Amr M. Kayid <amrkayid2027@gmail.com>
2018-09-17 13:48:56 +0200

    Initial commit

---
+++ graphics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  graphics
+//
+//  Created by Amr M. Kayid on 9/17/18.
+//  Copyright © 2018 Amr M. Kayid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
