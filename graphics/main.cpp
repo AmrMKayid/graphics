b5996906d1ebf3fffea7e0e1860af2e102a775b0
Amr M. Kayid <amrkayid2027@gmail.com>
2018-10-06 17:45:38 +0200

    [tut] initialization

--- graphics/main.cpp
+++ graphics/main.cpp
@@ -9,23 +9,41 @@
 #endif
 
 
-int main(int argc,char** argr)
-{
+void Display(void);
+
+int main(int argc, char** argr) {
+    
+    // init GLUT and create window
     glutInit(&argc, argr);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
     glutInitWindowPosition(50, 50);
+    glutInitWindowSize(500, 500);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glutCreateWindow("Space Invaders");
+    
+    // register callbacks
     glutDisplayFunc(Display);
-    // glutIdleFunc(Anim);
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+    
+    // enter GLUT event processing cycle
     glutMainLoop();
+    
+//    glutIdleFunc(Anim);
+//    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+//    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+    
+    
+    return 1;
 }
 
-void Display(void)
-{
-
+void Display(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glBegin(GL_TRIANGLES);
+    glVertex3f(-0.5,-0.5,0.0);
+    glVertex3f(0.5,0.0,0.0);
+    glVertex3f(0.0,0.5,0.0);
+    glEnd();
+    
+    glutSwapBuffers();
 }
 
 
